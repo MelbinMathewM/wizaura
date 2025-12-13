a36b65984efe669c826e38fc76fd398bb9a11eb0
Melbin Mathew <melbinppmathewp@gmail.com>
2025-12-13 09:52:04 +0530

    27th commit - Changed the next version to v15.5.9

--- src/components/contact/section.tsx
+++ src/components/contact/section.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 import { info } from "@/data/contact";
 
